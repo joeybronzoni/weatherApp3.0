7f37d368e8de6bd67e619aa52ffecd8aa46b51d4
joeybronzoni <josephskrzysowski@gmail.com>
2018-06-21 11:02:57 -0400

    Update geocode.js

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -4,7 +4,7 @@ const request = require('request');
 var geocodeAddress = (address, callback) => {
   let encodedAddress = encodeURIComponent(address);
   const baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-  const API_KEY = `AIzaSyCmYd0Kfz7J74884B2yJSe1AxOFzzYVRlI`;
+  const API_KEY = 'Whats your api key';
 
   request({
 	url: `${baseUrl}${encodedAddress}&key=${API_KEY}`,
@@ -30,23 +30,3 @@ var geocodeAddress = (address, callback) => {
 // export
 module.exports.geocodeAddress = geocodeAddress;
 
-
-
-/* NOTES:
-	  /   We no longer need to log these because we can use the callback() instead:
-		 const Address = JSON.stringify(response.body.results[0].formatted_address);
-		 const lat = JSON.stringify(response.body.results[0].geometry.location.lat);
-		 const lng = JSON.stringify(response.body.results[0].geometry.location.lng);
-	   /
-
-	  / We are passing undefined and 2 in the app.js so we don't need to call them here
-		const Address = JSON.stringify(response.body.results[0].formatted_address, undefined, 2);
-	  const lat = JSON.stringify(response.body.results[0].geometry.location.lat, undefined, 2);
-	  const lng = JSON.stringify(response.body.results[0].geometry.location.lng, undefined, 2);
-	  console.log('Address: ', Address);
-	  console.log('latitude: ', lat);
-	  console.log('longitude: ',lng);/
-
-	// When an invalid address gets called the error object does not get called, that is the response object that because the request object doesn't see it as an error hence the else if() statements to check the errors. With googlemaps api they have a status in the body that we can use
-
-*/
